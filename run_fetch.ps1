8cce75b508e07a6e35c8e26a222b508d4d711a23
Dmitriy <79217526217@ya.ru>
2026-02-18 13:31:10 +0300

    Delete run_fetch.ps1

--- run_fetch.ps1
+++
@@ -1,6 +0,0 @@
-# Загрузка документации Python с docs.python.org
-
-$ProjectRoot = $PSScriptRoot
-Set-Location $ProjectRoot
-pip install beautifulsoup4 -q 2>$null
-python scripts/fetch_python_docs.py
